1d54c28fd901485d4cabc1fc0e20ea19850b02d0
Tomasz Klim <github@tomaszklim.pl>
2018-10-24 13:27:57 +0200

    use new decode.sh script to decode inventory entries

--- cron/inspect.sh
+++ cron/inspect.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 . /opt/farm/scripts/init
 . /opt/farm/scripts/functions.custom
 
@@ -7,18 +7,8 @@ admin=`primary_admin_account`
 
 for server in `/opt/farm/ext/inspect-users/utils/get-hosts.sh`; do
 
-	if [[ $server =~ ^[a-z0-9.-]+$ ]]; then
-		server="$server::"
-	elif [[ $server =~ ^[a-z0-9.-]+[:][0-9]+$ ]]; then
-		server="$server:"
-	fi
-
-	host=$(echo $server |cut -d: -f1)
-	port=$(echo $server |cut -d: -f2)
-
-	if [ "$port" = "" ]; then
-		port=22
-	fi
+	host=`/opt/farm/ext/farm-manager/internal/decode.sh host $server`
+	port=`/opt/farm/ext/farm-manager/internal/decode.sh port $server`
 
 	sshkey=`/opt/farm/ext/keys/get-ssh-management-key.sh $host`
 
--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 /opt/farm/scripts/setup/extension.sh sf-versioning
+/opt/farm/scripts/setup/extension.sh sf-farm-manager
 /opt/farm/scripts/setup/extension.sh sf-php
 
 echo "setting up base directories and files"

f3df92fec5ad0693ab0c1d32ae6fd93b2cc72923
Tomasz Klim <github@tomaszklim.pl>
2020-02-14 01:44:18 +0100

    added container.hosts file

--- utils/get-hosts.sh
+++ utils/get-hosts.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 path=/etc/local/.farm
-cat $path/virtual.hosts $path/physical.hosts $path/cloud.hosts $path/lxc.hosts $path/workstation.hosts $path/problematic.hosts |grep -v "^#"
+cat $path/virtual.hosts $path/physical.hosts $path/cloud.hosts $path/container.hosts $path/lxc.hosts $path/workstation.hosts $path/problematic.hosts |grep -v "^#"

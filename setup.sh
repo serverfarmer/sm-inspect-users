85547b5421a41a61e2029a72a30962b8053b3e54
Tomasz Klim <github@tomaszklim.pl>
2020-03-23 12:11:08 +0100

    removed old transitional code

--- setup.sh
+++ setup.sh
@@ -12,8 +12,3 @@ chown root:www-data /var/cache/farm
 if ! grep -q /opt/farm/ext/inspect-users/cron /etc/crontab; then
 	echo "48 6 * * * root /opt/farm/ext/inspect-users/cron/inspect.sh" >>/etc/crontab
 fi
-
-# transitional code
-if grep -q /opt/farm/ext/farm-inspector/cron/users.sh /etc/crontab; then
-	sed -i -e "/\/opt\/farm\/ext\/farm-inspector\/cron\/users.sh/d" /etc/crontab
-fi

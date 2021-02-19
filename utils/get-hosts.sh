b2ba38019f347bf58ac90c80f77d8f6b5c5de474
Tomasz Klim <github@tomaszklim.pl>
2021-02-19 22:56:45 +0100

    use relative paths instead of /etc/local/.farm

--- utils/get-hosts.sh
+++ utils/get-hosts.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-path=/etc/local/.farm
+path=~/.farm
 cat $path/virtual.hosts $path/physical.hosts $path/cloud.hosts $path/container.hosts $path/lxc.hosts $path/workstation.hosts $path/problematic.hosts |grep -v "^#"
